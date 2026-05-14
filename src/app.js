53583ef8edee53f9f596cee53ee0365ee5639f7c
suraj project <sahoosuraj744@gmail.com>
2026-05-14 18:50:53 +0530

    fix: correct formatting and add missing routes for playlists and dashboard

--- src/app.js
+++ src/app.js
@@ -15,10 +15,13 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/tweets",tweetRouter );
+app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscrptions", subscriptionRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+
 //http://localhost:8000/api/v1/users/register
 export { app };
